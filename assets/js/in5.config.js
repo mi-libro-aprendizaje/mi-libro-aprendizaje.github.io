fd3d7d1d7f04209b664b8cd691e357d9fd37ee2b
Juan Eduardo Gomez <43454459+4gmz@users.noreply.github.com>
2021-06-15 16:17:46 -0500

    cambio de presentación libro

--- assets/js/in5.config.js
+++ assets/js/in5.config.js
@@ -31,7 +31,7 @@ var prefix = (function () {
 })();
 var pre = (document.createElement('div').style['WebkitTransform'] != undefined) ? '-webkit-' : '';
 var useSwipe = true;
-var pageMode = 'h';
+var pageMode = 'v';
 var pageW = 600, pageH = 800;
 var multifile = false;
 if(multifile) { 
